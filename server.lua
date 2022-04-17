4e103ee886a41908834f60e49ea42e3f1cc36303
Edoardo <102823453+Martuh@users.noreply.github.com>
2022-04-17 12:00:42 +0200

    Add files via upload

---
+++ server.lua
@@ -0,0 +1,17 @@
+--- PARTE SERVER BY MARTU
+
+ESX.RegisterServerCallback("martu:prendigruppo", function(source, cb)
+    local player = ESX.GetPlayerFromId(source)
+
+    if player ~= nil then
+        local playerGroup = player.getGroup()
+
+        if playerGroup ~= nil then 
+            cb(playerGroup)
+        else
+            cb("user")
+        end
+    else
+        cb("user")
+    end
+end)
