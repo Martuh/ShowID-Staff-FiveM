7c036519aba801da6441c6220127536171eb1b68
Edoardo <102823453+Martuh@users.noreply.github.com>
2022-04-17 11:57:56 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'adamant'
+game 'gta5'
+
+files {
+	'client/*.lua',
+	'server/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {,
+	'server/*.lua'
+}
